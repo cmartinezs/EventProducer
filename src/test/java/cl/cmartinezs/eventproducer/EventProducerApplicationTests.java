95404f8f86d0c19dda0e817c5dc7397bdf188a06
Carlos F. Martínez Sánchez <carlos.f.martinez+github@gmail.com>
2023-06-29 21:11:12 -0400

    first

---
+++ src/test/java/cl/cmartinezs/eventproducer/EventProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cmartinezs.eventproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
